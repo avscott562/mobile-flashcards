87c16d1fd1dafcce34765665ab9d3d403f1bdccf
Scottie <avscott@pacbell.net>
2021-01-18 01:15:35 -0800

    add state to quiz and increment/decrment functions

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 import { connect } from 'react-redux'
 import { CommonActions } from '@react-navigation/native'
 import Score from './Score'
@@ -8,9 +8,19 @@ import { mauve } from '../utils/colors'
 class Quiz extends Component {
     state = {
         displayAnswer: false,
+        cardIndex: 0,
+        totalCards: 1,
         correct: 0,
         incorrect: 0,
         quizComplete: false
+    }
+
+    componentDidMount() {
+        const { deck } = this.props
+
+        this.setState(() => ({
+            totalCards: deck.questions.length
+        }))
     }
 
     toggleAnswer = () => {
@@ -19,16 +29,58 @@ class Quiz extends Component {
         }))
     }
 
+    incrementScore = () => {
+        this.setState(() => ({
+            correct: this.state.correct + 1
+        }))
+
+        console.log('correct', this.state.correct)
+        console.log('wrong', this.state.incorrect)
+    }
+
+    decrementScore = () => {
+        this.setState(() => ({
+            incorrect: this.state.incorrect + 1
+        }))
+
+        console.log('correct', this.state.correct)
+        console.log('wrong', this.state.incorrect)
+    }
+
+    handleResult = () => {
+        const { correct, incorrect, totalCards } = this.state
+
+        let answered = correct + incorrect
+
+        if (answered < totalCards) {
+            this.setState(() => ({
+                displayAnswer: false,
+                cardIndex: this.state.cardIndex + 1
+            }))
+        } else {
+            this.setState(() => ({
+                quizComplete: true
+            }))
+        }
+    }
+
     render() {
-        const { displayAnswer, correct, incorrect, quizComplete } = this.state
+        const { 
+            displayAnswer, 
+            cardIndex,
+            totalCards, 
+            correct, 
+            quizComplete } = this.state
+        const { deck } = this.props
 
         return (
             <View>
                 <Text style={styles.header}> Quiz </Text>
+                <Text>{correct} / {totalCards}</Text>
                 <View style={styles.container}>
                     {!displayAnswer
                     ? <View>
-                        <Text style={styles.mainText}>What is the capitol of Louisiana?</Text>
+                        <Text style={styles.mainText}>{deck.questions[cardIndex].question}</Text>
                         <TouchableOpacity 
                           style={styles.button}
                           onPress={this.toggleAnswer}>
@@ -36,11 +88,15 @@ class Quiz extends Component {
                         </TouchableOpacity>
                       </View>
                     : <View>
-                        <Text style={styles.mainText}>Baton Rouge</Text>
-                        <TouchableOpacity style={styles.button}>
+                        <Text style={styles.mainText}>{deck.questions[cardIndex].answer}</Text>
+                        <TouchableOpacity 
+                          style={styles.button}
+                          onPress={this.incrementScore}>
                             <Text style={styles.buttonText}>Correct</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={styles.button}>
+                        <TouchableOpacity 
+                          style={styles.button}
+                          onPress={this.decrementScore}>
                             <Text style={styles.buttonText}>Incorrect</Text>
                         </TouchableOpacity>
                       </View>
@@ -48,7 +104,7 @@ class Quiz extends Component {
                     
                     {quizComplete && 
                     <View>
-                        <Text>3 of 5</Text>
+                        <Text>quiz completed? {quizComplete}</Text>
                         <Score />
                     </View>
                     }
@@ -102,4 +158,16 @@ const styles = StyleSheet.create({
     },
 })
 
-export default connect()(Quiz)
+function mapStateToProps (state, { route, navigation }) {
+    const { deckId } = route.params
+    const deck = state[deckId]
+    console.log('from quiz: ', deck)
+    return {
+        navigation,
+        route,
+        deck,
+        deckId
+    }
+}
+
+export default connect(mapStateToProps)(Quiz)
